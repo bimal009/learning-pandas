b8c2a4591fa8d37e91236130e04795db9e83334b
bimal009 <pandeybimal616@gmail.com>
2025-05-17 18:06:50 +0545

    Fix city name for ram in output.csv and add rows.py for data processing

---
+++ rows.py
@@ -0,0 +1,17 @@
+import pandas as pd
+# head()
+# tail()
+
+# data=pd.read_csv("sales_data_sample.csv",encoding="latin1")
+data=pd.read_csv("output.csv",encoding="latin1")
+# print(data.head(10))
+# print(data.tail(10))
+# print(data.info())
+# print(data)
+# print(data.describe())
+# print(data.shape)
+# print(data.columns)
+
+# print(data[["age","name"]])
+# print(data[data["age"]>20])
+print(data[(data["age"]>20) & (data["age"]<40) ])
